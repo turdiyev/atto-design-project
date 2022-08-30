8ce8e7160d5840b603d501cb5ad442fc109d68fc
Sherali <sherali.uz@gmail.com>
2022-08-30 16:29:22 +0500

    feat: dark storybook theme

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,6 @@
 import "../src/styles/index.css"
+import { themes } from '@storybook/theming';
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
@@ -6,5 +8,15 @@ export const parameters = {
       color: /(background|color)$/i,
       date: /Date$/,
     },
+  },
+  docs: {
+    theme: themes.dark,
+  },
+  backgrounds: {
+    default: 'Dark Mode',
+    values: [
+      { name: 'Dark Mode', value: '#1f2f7a' },
+      { name: 'Light Mode', value: '#fff' },
+    ],
   },
 }

550dcfcb917e875120961e67e7da75b6a3d1ad12
Aydos Sankibaev <aji3385@gmail.com>
2022-09-01 15:43:50 +0500

    Default Sidebar items are done

---
+++ src/components/layout/sidebar/Sidebar.tsx
@@ -0,0 +1,134 @@
+interface Props {
+}
+
+export default function Sidebar({ }: Props) {
+
+  return (
+    <div className={`w-full lg:w-fit  bg-black z-20   bg-opacity-30 fixed top-0 left-0 h-full`}>
+      <nav className={`bg-[#1F2A66] flex transition-hidden duration-75 lg:flex max-w-[280px] min-w-[280px] fixed lefy-0  h-full min-h-screen px-4 py-4 flex-col justify-between`}>
+
+        <div className="flex-col space-y-4">
+
+          <div className="flex flex-row space-x-2 items-center ml-2 py-2">
+            <h6 className="text-2xl font-bold text-white">ATTO</h6>
+          </div>
+
+          <ul className="flex flex-col space-y-2 mt-24">
+
+            <li className={`p-2.5 text-gray-400 rounded flex flex-row flex-nowrap space-x-4 items-center text-sm font-medium hover:bg-secondary hover:text-white group cursor-pointer`}>
+              <span>
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M8 14v3m4-3v3m4-3v3M3 21h18M3 10h18M3 7l9-4 9 4M4 10h16v11H4V10z" />
+                </svg>
+              </span>
+              <h6 className=" text-md font-medium">Поездки</h6>
+            </li>
+
+            <li className={`p-2.5 text-gray-400 rounded flex flex-row flex-nowrap space-x-4 items-center text-sm font-medium hover:bg-secondary hover:text-white group cursor-pointer`}>
+              <span>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="h-5 w-5" >
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 3v11.25A2.25 2.25 0 006 16.5h2.25M3.75 3h-1.5m1.5 0h16.5m0 0h1.5m-1.5 0v11.25A2.25 2.25 0 0118 16.5h-2.25m-7.5 0h7.5m-7.5 0l-1 3m8.5-3l1 3m0 0l.5 1.5m-.5-1.5h-9.5m0 0l-.5 1.5M9 11.25v1.5M12 9v3.75m3-6v6" />
+                </svg>
+              </span>
+              <h6 className=" text-md font-medium">Общие показатели</h6>
+            </li>
+
+            <li className={`p-2.5 text-gray-400 rounded flex flex-row flex-nowrap space-x-4 items-center text-sm font-medium hover:bg-secondary hover:text-white group cursor-pointer`}>
+              <span>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-5 h-5">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 18.75a60.07 60.07 0 0115.797 2.101c.727.198 1.453-.342 1.453-1.096V18.75M3.75 4.5v.75A.75.75 0 013 6h-.75m0 0v-.375c0-.621.504-1.125 1.125-1.125H20.25M2.25 6v9m18-10.5v.75c0 .414.336.75.75.75h.75m-1.5-1.5h.375c.621 0 1.125.504 1.125 1.125v9.75c0 .621-.504 1.125-1.125 1.125h-.375m1.5-1.5H21a.75.75 0 00-.75.75v.75m0 0H3.75m0 0h-.375a1.125 1.125 0 01-1.125-1.125V15m1.5 1.5v-.75A.75.75 0 003 15h-.75M15 10.5a3 3 0 11-6 0 3 3 0 016 0zm3 0h.008v.008H18V10.5zm-12 0h.008v.008H6V10.5z" />
+                </svg>
+
+              </span>
+              <h6 className=" text-md font-medium">Наличные оплаты</h6>
+            </li>
+
+            <li className={`p-2.5 text-gray-400 rounded flex flex-row flex-nowrap space-x-4 items-center text-sm font-medium hover:bg-secondary hover:text-white group cursor-pointer`}>
+              <span>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-5 h-5">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 002.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 00-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 00.75-.75 2.25 2.25 0 00-.1-.664m-5.8 0A2.251 2.251 0 0113.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25zM6.75 12h.008v.008H6.75V12zm0 3h.008v.008H6.75V15zm0 3h.008v.008H6.75V18z" />
+                </svg>
+              </span>
+              <h6 className=" text-md font-medium">История</h6>
+            </li>
+          </ul>
+
+
+          <ul className="flex flex-col space-y-2 mt-24">
+
+            <li className="p-2 py-0 rounded flex flex-row flex-nowrap space-x-2 items-center text-sm font-medium text-slate-500 hover:text-blue-500 group">
+              <p className="text-sm font-bold text-white">Отчеты</p>
+            </li>
+            <li className="w-full border-b border-white/20" />
+
+            <li className={`p-2.5 text-gray-400 rounded flex flex-row flex-nowrap space-x-4 items-center text-sm font-medium hover:bg-secondary hover:text-white group cursor-pointer`}>
+              <span>
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M8 14v3m4-3v3m4-3v3M3 21h18M3 10h18M3 7l9-4 9 4M4 10h16v11H4V10z" />
+                </svg>
+              </span>
+              <h6 className=" text-md font-medium">Автобусы</h6>
+            </li>
+
+            <li className={`p-2.5 text-gray-400 rounded flex flex-row flex-nowrap space-x-4 items-center text-sm font-medium hover:bg-secondary hover:text-white group cursor-pointer`}>
+              <span>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="h-5 w-5" >
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 3v11.25A2.25 2.25 0 006 16.5h2.25M3.75 3h-1.5m1.5 0h16.5m0 0h1.5m-1.5 0v11.25A2.25 2.25 0 0118 16.5h-2.25m-7.5 0h7.5m-7.5 0l-1 3m8.5-3l1 3m0 0l.5 1.5m-.5-1.5h-9.5m0 0l-.5 1.5M9 11.25v1.5M12 9v3.75m3-6v6" />
+                </svg>
+              </span>
+              <h6 className=" text-md font-medium">Маршрутки</h6>
+            </li>
+
+            <li className={`p-2.5 text-gray-400 rounded flex flex-row flex-nowrap space-x-4 items-center text-sm font-medium hover:bg-secondary hover:text-white group cursor-pointer`}>
+              <span>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-5 h-5">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 18.75a60.07 60.07 0 0115.797 2.101c.727.198 1.453-.342 1.453-1.096V18.75M3.75 4.5v.75A.75.75 0 013 6h-.75m0 0v-.375c0-.621.504-1.125 1.125-1.125H20.25M2.25 6v9m18-10.5v.75c0 .414.336.75.75.75h.75m-1.5-1.5h.375c.621 0 1.125.504 1.125 1.125v9.75c0 .621-.504 1.125-1.125 1.125h-.375m1.5-1.5H21a.75.75 0 00-.75.75v.75m0 0H3.75m0 0h-.375a1.125 1.125 0 01-1.125-1.125V15m1.5 1.5v-.75A.75.75 0 003 15h-.75M15 10.5a3 3 0 11-6 0 3 3 0 016 0zm3 0h.008v.008H18V10.5zm-12 0h.008v.008H6V10.5z" />
+                </svg>
+
+              </span>
+              <h6 className=" text-md font-medium">Метро</h6>
+            </li>
+
+            <li className={`p-2.5 text-gray-400 rounded flex flex-row flex-nowrap space-x-4 items-center text-sm font-medium hover:bg-secondary hover:text-white group cursor-pointer`}>
+              <span>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-5 h-5">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 002.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 00-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 00.75-.75 2.25 2.25 0 00-.1-.664m-5.8 0A2.251 2.251 0 0113.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25zM6.75 12h.008v.008H6.75V12zm0 3h.008v.008H6.75V15zm0 3h.008v.008H6.75V18z" />
+                </svg>
+              </span>
+              <h6 className=" text-md font-medium">Общий отчет</h6>
+            </li>
+
+            <li className={`p-2.5 text-gray-400 rounded flex flex-row flex-nowrap space-x-4 items-center text-sm font-medium hover:bg-secondary hover:text-white group cursor-pointer`}>
+              <span>
+
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-5 h-5">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 7.5V6.108c0-1.135.845-2.098 1.976-2.192.373-.03.748-.057 1.123-.08M15.75 18H18a2.25 2.25 0 002.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 00-1.123-.08M15.75 18.75v-1.875a3.375 3.375 0 00-3.375-3.375h-1.5a1.125 1.125 0 01-1.125-1.125v-1.5A3.375 3.375 0 006.375 7.5H5.25m11.9-3.664A2.251 2.251 0 0015 2.25h-1.5a2.251 2.251 0 00-2.15 1.586m5.8 0c.065.21.1.433.1.664v.75h-6V4.5c0-.231.035-.454.1-.664M6.75 7.5H4.875c-.621 0-1.125.504-1.125 1.125v12c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V16.5a9 9 0 00-9-9z" />
+                </svg>
+
+              </span>
+              <h6 className=" text-md font-medium">Тарифы</h6>
+            </li>
+
+          </ul>
+
+        </div>
+
+        <div className="flex-col space-y-4 mb-0">
+
+          <ul className="flex flex-col space-y-2 ">
+            <li className={`p-2.5 text-gray-400 rounded flex flex-row flex-nowrap space-x-4 items-center text-sm font-medium hover:bg-secondary hover:text-white group cursor-pointer`}>
+              <span>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-5 h-5">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M12 9l-3 3m0 0l3 3m-3-3h12.75" />
+                </svg>
+              </span>
+              <h6 className=" text-md font-medium">Выйти</h6>
+            </li>
+          </ul>
+
+        </div>
+
+      </nav>
+    </div>
+  )
+}

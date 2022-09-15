6ab1d1e83afa8d963f4ba98ae9767637a554a6c4
Aydos Sankibaev <aji3385@gmail.com>
2022-09-15 16:41:55 +0500

    Tooltip comments are removed

--- src/components/form/tooltip/Tooltip.stories.tsx
+++ src/components/form/tooltip/Tooltip.stories.tsx
@@ -2,22 +2,18 @@ import { ComponentStory, ComponentMeta } from "@storybook/react";
 
 import Tooltip from "./Tooltip";
 
-// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
   title: "Elements/Tooltip",
   component: Tooltip,
-  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
   argTypes: {},
   parameters: {
     layout: "centered",
   },
 } as ComponentMeta<typeof Tooltip>;
 
-// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
 const Template: ComponentStory<typeof Tooltip> = (args) => <Tooltip {...args} />;
 
 export const top = Template.bind({});
-// More on args: https://storybook.js.org/docs/react/writing-stories/args
 top.args = {
   position: 'top'
 };
@@ -28,7 +24,6 @@ right.args = {
 };
 
 export const bottom = Template.bind({});
-// More on args: https://storybook.js.org/docs/react/writing-stories/args
 bottom.args = {
   position: 'bottom'
 };

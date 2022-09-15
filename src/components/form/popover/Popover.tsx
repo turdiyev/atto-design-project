ea20337fa6d20068dd22f13120a3df2a552a7d93
Aydos Sankibaev <aji3385@gmail.com>
2022-09-15 17:12:20 +0500

    some css positions bugs are fixed

--- src/components/form/popover/Popover.tsx
+++ src/components/form/popover/Popover.tsx
@@ -2,7 +2,8 @@ import { useState } from "react"
 import Button from "../button/Button"
 
 type Props = {
-  variant: 'Hover' | 'Click' | 'Focus'
+  variant: 'Hover' | 'Click' | 'Focus',
+  position: 'top' | 'bottom' | 'left' | 'right'
 }
 
 export default function Popover({
@@ -11,7 +12,7 @@ export default function Popover({
 
 }: Props) {
 
-  const [open, setOpen] = useState<Boolean>(false)
+  const [open, setOpen] = useState<boolean>(false)
 
   return (
     <div className="relative">
@@ -36,23 +37,30 @@ export default function Popover({
         {variant === 'Focus' && 'Focus to activate'}
       </Button>
 
-      <div className={`shadow-md transition duration-300 ease-in-out translate-x-[-24%] absolute
-          bottom-[70px] flex flex-col opacity-0 w-[400px]
-        bg-white p-4 rounded 
+      <div className={`shadow-md transition duration-300 ease-in-out 
+          absolute
+          bottom-[70px]
+          left-1/2 transform -translate-x-1/2
+
+          flex flex-col opacity-0 w-[400px] bg-white  rounded
 
         ${variant === 'Hover' && 'peer-hover:opacity-100'}
         ${(variant === 'Click' && open) && 'opacity-100'}
         ${(variant === 'Focus' && open) && 'opacity-100'}
         
         `}>
-        <h1 className="text-md font-semibold mb-2 mt-0 ml-0 ">
-          Title to context
-        </h1>
-        <p className="text-sm font-semibold text-gray-600 ">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium dolore voluptatum illum ex?
-        </p>
-        {/* TRIANGLE */}
-        <div className="w-3 h-3 border-solid absolute bottom-[-12px] left-[46%] border-t-white border-t-8 border-x-transparent border-x-8 border-b-0"></div>
+        <div className="relative p-4 rounded">
+          <h1 className="text-md font-semibold mb-2 mt-0 ml-0 ">
+            Title to context
+          </h1>
+          <p className="text-sm font-semibold text-gray-600 ">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium dolore voluptatum illum ex?
+          </p>
+          {/* TRIANGLE */}
+          <div className="w-3 h-3 border-solid 
+            absolute bottom-[-12px] left-1/2 transform -translate-x-1/2
+          border-t-white border-t-8 border-x-transparent border-x-8 border-b-0"></div>
+        </div>
       </div>
 
     </div >
